df7a71a6ec0c87422f5e0e101acd3e8e09aed796
tyagi-nishant <mailmenishanttyagi@gmail.com>
2024-08-23 18:29:45 +0530

    https replace

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MemoryGame from './MemoryGame.jsx';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <MemoryGame />
+    </div>
+  );
+}
+
+export default App;
